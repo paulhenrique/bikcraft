52b0a8ce8e24003320026540d33c9f3a229baa58
Paulo Henrique <phvcandido@gmail.com>
2021-05-05 13:53:20 -0300

    Adicionando simple slide

---
+++ js/main.js
@@ -0,0 +1,15 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: 'portfolio', // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides,
+    transition: true,
+    nav: true
+  });
+}
